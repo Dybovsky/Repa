ce48dc8d5e36835ba2060e07f47549a77b53e2e7
Tom <64869112+Dybovsky@users.noreply.github.com>
2020-10-10 15:26:06 +0300

    add index

---
+++ save_page.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+from horoscope import generate_prophecies
+from datetime import datetime as dt
+
+
+def generate_page(head, body):
+    page = f"""<html>
+        {head}
+        {body}
+</html>"""
+    return page
+
+
+def generate_head(title):
+    head = f"""        <meta charset='utf-8'> 
+            <title> {title} </title>"""
+    return f"""<head> 
+    {head} 
+        </head>"""
+
+
+# def generate_body(header, paragraphs):
+#     body = f"""        <h1> {header} </h1>"""
+#     #par = paragraphs()
+#     i = 0
+#     while i < len(paragraphs):
+#         body = f"""{body} 
+#             <p> {paragraphs[i]} 
+#             </p>"""
+#         i = i + 1
+#     return f"""<body> 
+#     {body} 
+#         </body>"""
+
+def generate_body(header, paragraphs):
+    body = "<h1>" + header + "</h1>"
+    # par = paragraphs
+    for p in paragraphs:
+        body = body + f"<p>{p}</p>"
+    return f"<body>{body}  {link_about}  </body>"
+
+
+
+def save_page(output, title, header, paragraphs):
+    fp = open(output, "w")
+    today = dt.now().date()
+    page = generate_page(
+        head=generate_head(title),
+        body=generate_body(header=header, paragraphs=paragraphs)
+        )
+    print(page, file=fp)
+    fp.close()
+
+
+link_about = '<a href="about.html">How it works</a>'
+today = dt.now().date()
+save_page(
+    output="index.html",
+	title="Horoscop for today",
+	header="What day " + str(today) + " prepared for us",
+	paragraphs=generate_prophecies()
+
+	)
